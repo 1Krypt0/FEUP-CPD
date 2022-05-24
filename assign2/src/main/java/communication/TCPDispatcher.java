f336207fd684b847097a0bbb32b718365765d846
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-24 11:17:31 +0100

    Add dispatcher for TCP messages

---
+++ assign2/src/main/java/communication/TCPDispatcher.java
@@ -0,0 +1,42 @@
+package communication;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import store.Store;
+
+public class TCPDispatcher extends Thread {
+    private final int port;
+    private final ExecutorService executorService;
+    private final byte[] buf;
+    private final Store store;
+
+    public TCPDispatcher(int port, Store store) throws IOException {
+        this.port = port;
+        this.executorService = Executors.newCachedThreadPool();
+        this.buf = new byte[512];
+        this.store = store;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+                Socket socket = serverSocket.accept();
+                InputStream stream = socket.getInputStream();
+                int length = stream.read(buf);
+                executorService.submit(new MessageHandler(buf, length, store));
+            } catch (IOException e) {
+                System.out.println("Error " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+}
