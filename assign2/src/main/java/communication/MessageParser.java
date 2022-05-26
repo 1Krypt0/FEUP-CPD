28b7a75e611c458d3e221866880585c205df53d1
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-26 18:42:53 +0100

    Add parser to create messages according to header

---
+++ assign2/src/main/java/communication/MessageParser.java
@@ -0,0 +1,26 @@
+package communication;
+
+import communication.messages.Message;
+import store.Node;
+
+/**
+ * Class that will be run inside the TCP and {@link MulticastDispatcher} Will
+ * parse the message and handle with the appropriate one
+ */
+public class MessageParser implements Runnable {
+
+    private final byte[] msg;
+    private final Node node;
+
+    public MessageParser(byte[] msg, Node node) {
+        this.msg = msg;
+        this.node = node;
+    }
+
+    @Override
+    public void run() {
+        Message message = Message.parseMessage(msg, node);
+
+        message.handleMessage();
+    }
+}
