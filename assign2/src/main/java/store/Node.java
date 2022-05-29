a88d0eb78c6cb9093a2344608e34b8325b9f0ffe
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-29 21:23:12 +0100

    Add dispatchers to Node class

--- assign2/src/main/java/store/Node.java
+++ assign2/src/main/java/store/Node.java
@@ -1,5 +1,40 @@
 package store;
 
+import java.io.IOException;
+
+import communication.MulticastDispatcher;
+import communication.TCPDispatcher;
+
 public class Node {
 
+  private final MulticastDispatcher multicastDispatcher;
+  private final TCPDispatcher tcpDispatcher;
+
+  public static void main(final String[] args) {
+    if (args.length != 4) {
+      System.out.println("Usage: java Store <IP_mcast_addr> <IP_mcast_port> <node_id>  <Store_port>");
+    } else {
+      try {
+        final Node node = new Node(args);
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        System.out.println("Error creating node: " + e.getMessage());
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public Node(final String[] args) throws NumberFormatException, IOException {
+    this.multicastDispatcher = new MulticastDispatcher(args[0], Integer.parseInt(args[1]), this);
+    final Thread multicastThread = new Thread(this.multicastDispatcher);
+    multicastThread.start();
+
+    this.tcpDispatcher = new TCPDispatcher(Integer.parseInt(args[3]), this);
+    final Thread tcpThread = new Thread(this.tcpDispatcher);
+    tcpThread.start();
+
+    System.out.println("Dispatchers have been initialized");
+
+  }
 }
