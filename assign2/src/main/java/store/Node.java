d7cd3dde99647dbbdd16c69e0e9992ea625ee32e
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-30 10:43:39 +0100

    Improve store

--- assign2/src/main/java/store/Node.java
+++ assign2/src/main/java/store/Node.java
@@ -3,29 +3,54 @@ package store;
 import communication.MulticastDispatcher;
 import communication.TCPDispatcher;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Node {
+    private static final String LOCALHOST = "localhost";
 
-    private final MulticastDispatcher multicastDispatcher;
-    private final TCPDispatcher tcpDispatcher;
+    private final int nodeID;
 
-    public static void main(final String[] args) throws InterruptedException {
+    private final HashMap<Integer, String> clusterIPs;
+    private final HashMap<Integer, Integer> clusterPorts;
+    private final List<Integer> clusterIDs;
+
+    private MulticastDispatcher multicastDispatcher;
+    private TCPDispatcher tcpDispatcher;
+
+    public static void main(String[] args) throws InterruptedException {
         if (args.length != 4) {
             System.out.println("Usage: java Store <IP_mcast_addr> <IP_mcast_port> <node_id>  <Store_port>");
         } else {
             try {
                 final Node node = new Node(args);
+                System.out.println("Dispatchers have been initialized");
             } catch (NumberFormatException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                System.out.println("Error creating node: " + e.getMessage());
                 e.printStackTrace();
             }
         }
     }
 
-    public Node(final String[] args) throws NumberFormatException, IOException, InterruptedException {
+    public Node(String[] args) {
+
+        this.nodeID = Integer.parseInt(args[2]);
+
+        this.clusterIPs = new HashMap<Integer, String>();
+        this.clusterPorts = new HashMap<Integer, Integer>();
+        this.clusterIDs = new ArrayList<Integer>();
+
+        try {
+            this.initDispatchers(args);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void initDispatchers(String[] args) throws NumberFormatException, IOException {
         this.multicastDispatcher = new MulticastDispatcher(args[0], Integer.parseInt(args[1]), this);
         final Thread multicastThread = new Thread(this.multicastDispatcher);
         multicastThread.start();
@@ -33,16 +58,10 @@ public class Node {
         this.tcpDispatcher = new TCPDispatcher(Integer.parseInt(args[3]), this);
         final Thread tcpThread = new Thread(this.tcpDispatcher);
         tcpThread.start();
-
-        System.out.println("Dispatchers have been initialized");
-
-        System.out.println(Arrays.toString(args));
-
-        if (args[3].equals("8082")) {
-            String msg = "This is a really awesome test message";
-
-            this.multicastDispatcher.sendMessage(msg.getBytes());
-        }
     }
 
+    public void handleElection() {
+        final String electionMessage = "ELECTION" + " id-" + Integer.toString(this.nodeID);
+
+    }
 }
