7d455c47eaf1d2db5547a9f5b55f3130743a72ca
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-30 19:07:06 +0100

    Add missing parameters to join Message handlers

--- assign2/src/main/java/communication/messages/JoinMessage.java
+++ assign2/src/main/java/communication/messages/JoinMessage.java
@@ -15,14 +15,18 @@ public class JoinMessage extends Message {
     @Override
     public void handleMessage() {
         int senderID = Integer.parseInt(this.header[1].split(":")[1]);
-        int membershipCounter = Integer.parseInt(this.header[2].trim().split(":")[1]);
+        int membershipCounter = Integer.parseInt(this.header[2].split(":")[1]);
+        int port = Integer.parseInt(this.header[3].split(":")[1]);
+        String ip = this.header[4].trim().split(":")[1];
         System.out.println("HANDLING JOIN");
         System.out.println("Sender ID is " + senderID);
         System.out.println("Membershup counter is " + membershipCounter);
-        node.receiveJoinMessage(senderID, membershipCounter);
+        System.out.println("Port is " + port);
+        System.out.println("IP is " + ip);
+        node.receiveJoinMessage(senderID, membershipCounter, ip, port);
     }
 
-    public static byte[] composeMessage(int id, int membershipCounter) {
-        return ("JOIN id:" + id + " membership:" + membershipCounter).getBytes();
+    public static byte[] composeMessage(int id, int membershipCounter, String ip, int port) {
+        return ("JOIN id:" + id + " membership:" + membershipCounter + " port:" + port + " ip:" + ip).getBytes();
     }
 }
