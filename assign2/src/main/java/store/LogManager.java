5465331bab017afc66f7bf4499845651c9f4e3e2
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-30 18:01:00 +0100

    Create Log Manager class

---
+++ assign2/src/main/java/store/LogManager.java
@@ -0,0 +1,82 @@
+package store;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LogManager {
+
+    private static final String logsDir = "/../../logs/";
+    private final String logFile;
+
+    public LogManager(int nodeID) {
+        String localDir = System.getProperty("user.dir");
+        String logDir = localDir + logsDir + nodeID;
+
+        Path logPath = Paths.get(logDir);
+        try {
+            Files.createDirectories(logPath);
+        } catch (IOException e) {
+            System.out.println("Error creating log directory: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        this.logFile = localDir + logsDir + nodeID + "/log.txt";
+    }
+
+    public void writeToLog(String logMessage) {
+        try {
+            FileWriter fileWriter = new FileWriter(this.logFile, true);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            writer.write(logMessage);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> get32MostRecentLogMessages() {
+        List<String> log = getLog();
+        Collections.reverse(log);
+
+        return log.subList(0, 32);
+
+    }
+
+    public List<String> getLog() {
+        File logFile = new File(this.logFile);
+        List<String> log = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(logFile));
+            String buf;
+            while ((buf = reader.readLine()) != null) {
+                System.out.println("Log line is: " + buf);
+                log.add(buf);
+            }
+            reader.close();
+            return log;
+        } catch (FileNotFoundException e) {
+            System.out.println("Error reading log File: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error closing log File: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+}
