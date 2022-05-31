056d5f224d4a1d086b0da4960e9e3adf6458d8d5
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-31 15:16:54 +0100

    Improve log to only write new messages

--- assign2/src/main/java/store/LogManager.java
+++ assign2/src/main/java/store/LogManager.java
@@ -11,8 +11,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import communication.messages.Message;
 
 public class LogManager {
 
@@ -32,27 +35,58 @@ public class LogManager {
         }
 
         this.logFile = localDir + logsDir + nodeID + "/log.txt";
+        File file = new File(this.logFile);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error creating log file: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public void writeToLog(String logMessage) {
         // TODO: Check for repeated messages. Add method to check if message already
         // exists
-        try {
-            FileWriter fileWriter = new FileWriter(this.logFile, true);
-            BufferedWriter writer = new BufferedWriter(fileWriter);
-            writer.write(logMessage);
-            writer.newLine();
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
-            e.printStackTrace();
+        List<String> recentLogEntries = get32MostRecentLogMessages();
+        String[] individualLogMessages = logMessage.split("\n");
+        for (String individualMessage : individualLogMessages) {
+            if (!recentLogEntries.contains(individualMessage)) {
+                try {
+                    FileWriter fileWriter = new FileWriter(this.logFile, true);
+                    BufferedWriter writer = new BufferedWriter(fileWriter);
+                    writer.write(individualMessage + "\n");
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("Error writing to file: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("The message" + individualMessage + " is repeated!");
+            }
         }
     }
 
     public List<String> get32MostRecentLogMessages() {
-        List<String> log = getLog();
-        Collections.reverse(log);
-        return log.subList(0, log.size() < 32 ? log.size() : 32);
+        File logFile = new File(this.logFile);
+        List<String> log = new ArrayList<>();
+        int counter = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(logFile));
+            String buf;
+            while ((buf = reader.readLine()) != null && counter < 32) {
+                log.add(buf);
+                counter++;
+            }
+            reader.close();
+            return log;
+        } catch (FileNotFoundException e) {
+            System.out.println("Error reading log File: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error closing log File: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public List<String> getLog() {
