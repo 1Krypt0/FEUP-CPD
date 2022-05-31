327ea67bd832f31767c8fa5bb3f4af847f2c79df
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-31 10:07:55 +0100

    Add method to compute union of two lists

--- assign2/src/main/java/utils/Utils.java
+++ assign2/src/main/java/utils/Utils.java
@@ -2,6 +2,10 @@ package utils;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Utils {
     public static byte[] calculateHash(byte[] value) {
@@ -14,5 +18,12 @@ public class Utils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static List<Integer> getListUnion(List<Integer> l1, List<Integer> l2) {
+        Set<Integer> union = new HashSet<>();
+        union.addAll(l1);
+        union.addAll(l2);
+        return new ArrayList<Integer>(union);
     }
 }
